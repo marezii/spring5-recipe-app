5644306639257ab01e7d94becd038bfb3091182d
marezi <markoilic22122012@gmail.com>
2020-10-07 10:53:03 +0200

    Bootstrap CSS added

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,12 +1,14 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"notes"})
 @Entity
 public class Recipe {
 
